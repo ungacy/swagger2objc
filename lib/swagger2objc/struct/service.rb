1f5f51ce19ef1280e5033dee470ee4312620d65f
ungacy <ungacy@126.com>
2018-02-28 17:18:47 +0800

    Service

--- lib/swagger2objc/struct/service.rb
+++ lib/swagger2objc/struct/service.rb
@@ -2,17 +2,20 @@ require 'swagger2objc/struct/model'
 module Swagger2objc
   module Struct
     class Service < Base
+      # json中对应的键值
       attr_reader :basePath
       attr_reader :definitions
-      attr_reader :common
       attr_reader :host
-      attr_reader :tags
       attr_reader :info
-      attr_accessor :paths
+      attr_reader :paths
       attr_reader :swagger
-      attr_accessor :only
-      attr_reader :controllers
-      attr_reader :name
+      attr_reader :tags
+
+      #custom
+      attr_reader :only
+      attr_reader :controllers #Resources or Controllers
+      #[{"name":"web","location":"/web/v2/api-docs","swaggerVersion":"2.0"}
+      attr_reader :name # from #{url}/servers structs
 
       def initialize(hash = {}, only, name)
         hash.each do |k, v|
@@ -27,11 +30,7 @@ module Swagger2objc
       end
 
       def setup
-        model_type = Swagger2objc::Config::MODEL
-        model_class_prefix = Swagger2objc::Configure.config[Swagger2objc::Config::CLASS_PREFIX][model_type]
         router_map = Swagger2objc::Configure.config[Swagger2objc::Config::ROUTER]
-        filter_array = Swagger2objc::Configure.config[Swagger2objc::Config::FILTER]
-        filter_array = [] if filter_array.nil?
         if definitions
           definitions.delete('Null')
           definitions.delete('Timestamp')
@@ -45,7 +44,6 @@ module Swagger2objc
             dict.each do |method, operation_hash|
               if operation_hash['tags']
                 controller_key = operation_hash['tags'].first
-                next if filter_array.include?(controller_key)
               else
                 next
               end
@@ -56,8 +54,6 @@ module Swagger2objc
               category = 'Audit' if category.include?('Audit')
               category = category[0].upcase + category[1..-1]
               category.gsub!(/\-\w/) { |match| match[1].upcase }
-              next if filter_array.include?(category)
-              next if category == 'Test'
               category = 'Notification' if category == 'ExternalChannel' || category == 'PushNotification'
               category = 'File' if category == 'AppFile'
               category = 'Login' if category == 'IdentityAudit' || category == 'VerifyCode'
