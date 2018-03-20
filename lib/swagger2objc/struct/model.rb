c19ef4ed953240b91acb44f9b8ae2da78d1e2aa2
ungacy <ungacy@126.com>
2018-03-20 15:20:54 +0800

    add_property

--- lib/swagger2objc/struct/model.rb
+++ lib/swagger2objc/struct/model.rb
@@ -1,4 +1,5 @@
 require 'swagger2objc/struct/property'
+require 'swagger2objc/config'
 module Swagger2objc
   module Struct
     class Model < Base
@@ -18,6 +19,16 @@ module Swagger2objc
           property.service = service
           property.required = @required.include?(key) if @required
           hash[key] = property
+        end
+        add_property = Swagger2objc::Configure.config['add_property'][id]
+        if add_property
+          add_property.each do |key, item|
+            property = Property.new(item)
+            property.name = key
+            property.service = service
+            property.required = @required.include?(key) if @required
+            hash[key] = property
+          end
         end
         @properties = hash
       end
