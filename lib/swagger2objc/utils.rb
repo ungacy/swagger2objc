e116445bc69b38129c10d27746bd9d0e0572a086
ungacy <ungacy@126.com>
2018-04-08 13:00:34 +0800

    ChatGroup

--- lib/swagger2objc/utils.rb
+++ lib/swagger2objc/utils.rb
@@ -55,8 +55,12 @@ module Swagger2objc
       if category == 'Message' || category == 'Permission' || category == 'Audit'
         short_name = operationid[0].capitalize + operationid[1..-1]
         short_name.gsub!(/By.*/, '')
-        short_name.sub!('/', '')
+
         return class_prefix + category + short_name
+      end
+      if category == 'ChatGroup'
+        puts operationid
+        result.sub!('ChatGroupChatGroup', 'ChatGroup')
       end
       # puts 'result : ' + result
       class_prefix + result
